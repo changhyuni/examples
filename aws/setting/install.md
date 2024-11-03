71567dbcf0ab5535a192d653803afe2d3e9297f1
june <bhs9610@naver.com>
2024-11-03 18:54:12 +0900

    add terraform

--- aws/setting/install.md
+++ aws/setting/install.md
@@ -3,3 +3,5 @@ aws-nuke  : https://github.com/rebuy-de/aws-nuke?tab=readme-ov-file#install
 aws-cli   : https://docs.aws.amazon.com/ko_kr/cli/latest/userguide/getting-started-install.html
 python3   : https://www.python.org/downloads/
 vscode    : https://code.visualstudio.com/download
+terraform : https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
+vscode extension: terraform, python3, gitignore
---
+++ aws/terraform/workflow/vpc/vpc.tf
@@ -0,0 +1,24 @@
+# Terraform Settings Block
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = "~> 3.0"
+        # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+# Resource Block
+resource "aws_vpc" "project1vpc" {
+    cidr_block = "192.168.0.0/24"
+    tags = {
+        Name = "FirstVPC"
+    }
+}

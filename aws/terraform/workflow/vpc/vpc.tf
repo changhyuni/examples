fa94eb76d7b4c5e01ce24773803996e74c664ffc
june <bhs9610@naver.com>
2024-11-03 21:22:17 +0900

    add terraform

--- aws/terraform/workflow/vpc/vpc.tf
+++ aws/terraform/workflow/vpc/vpc.tf
@@ -1,11 +1,10 @@
-# Terraform Settings Block
+# Settings Block
 terraform {
   required_version = "~> 1.0"
   required_providers {
     aws = {
         source  = "hashicorp/aws"
         version = "~> 3.0"
-        # Optional but recommended in production
     }
   }
 }

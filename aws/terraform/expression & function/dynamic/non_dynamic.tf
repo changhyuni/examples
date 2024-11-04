2b2a327f077ad5c90f6e67f1f20086c22941feee
june <bhs9610@naver.com>
2024-11-04 22:36:22 +0900

    add function

--- aws/terraform/expression & function/dynamic/non_dynamic.tf
+++ aws/terraform/expression & function/dynamic/non_dynamic.tf
@@ -1,20 +1,20 @@
-resource "aws_security_group" "example" {
-  name   = "example-sg"
-  vpc_id = "vpc-123456"
+# resource "aws_security_group" "example" {
+#   name   = "example-sg"
+#   vpc_id = "vpc-123456"
 
-  # 인바운드 규칙 1
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   # 인바운드 규칙 1
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  # 인바운드 규칙 2
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   # 인바운드 규칙 2
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }

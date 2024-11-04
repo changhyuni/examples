3ac4c40adcfb47764ea81bba203ed1b1dad3f7a9
june <bhs9610@naver.com>
2024-11-05 00:41:31 +0900

    add backend

--- aws/terraform/backend/data_source/main.tf
+++ aws/terraform/backend/data_source/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      # version = "5.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
 data "aws_vpc" "example" {
   default = true
 }
